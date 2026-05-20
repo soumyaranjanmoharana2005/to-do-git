f90490fb52c5e506cf78907f604ac9e4dde0194b
Soumya Ranjan Moharana <soumyaranjanmoharana8659@gmail.com>
2026-05-20 13:30:16 +0000

    added index.html and corrected errors

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 # Flask: main class, render_template for HTML file in index file, request: que te dejá ver the data from server, redirect: to a diff URL
 # url_for: for redirect
 
-import database #database_fn
+from database import get_connection, init_db#database_fn
 
 app= Flask(__name__)  #app object
 
@@ -22,7 +22,7 @@ def add_todo():
     task= request.form.get("task") #getting the task from the post, j'crois
     if bool(task)== True:
         conn= get_connection()
-        conn.execute("INSERT INTO todos(tasks) VALUES (?)", (task,))  # (task,) is a single tuple and «?» is there for SQL to treat it as string...(j'crois)
+        conn.execute("INSERT INTO todos(task) VALUES (?)", (task,))  # (task,) is a single tuple and «?» is there for SQL to treat it as string...(j'crois)
         conn.commit()
         conn.close()
     return redirect(url_for("index"))
@@ -35,10 +35,10 @@ def toggle_todo(id):   #É the toggle
     conn.close()
     return redirect(url_for("index"))
 
-@app.route("/delete/<int:id>": methods= ["POST"])
+@app.route("/delete/<int:id>", methods= ["POST"])
 def delete_todo(id):
     conn= get_connection()
-    conn.execute("DELETE FROM todo where id = ?", (id,))
+    conn.execute("DELETE FROM todos where id = ?", (id,))
     conn.commit()
     conn.close()
     return redirect(url_for("index"))
--- database.py
+++ database.py
@@ -1,6 +1,18 @@
 import sqlite3
 
 def get_connection():
-    conn=sqlite3.connect(todos.db)    #N'oublie pas, it's for connection
-    conn.row_factory= sqlite3.Row     #row_factory is an attribute and we are setting that attributes to a value
-
+    conn=sqlite3.connect("todos.db")    #N'oublie pas, it's for connection
+    conn.row_factory= sqlite3.Row      #row_factory is an attribute and we are setting that attributes to a value
+    return conn
+def init_db():
+    conn= get_connection()
+    conn.execute("""
+         CREATE TABLE IF NOT EXISTS todos (
+            id      INTEGER PRIMARY KEY AUTOINCREMENT,
+            task    TEXT NOT NULL,
+            done    INTEGER DEFAULT 0,
+            created TEXT DEFAULT (datetime('now'))
+           )
+        """)
+    conn.commit()
+    conn.close()
