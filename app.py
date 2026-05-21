900196dfe9ce8c6a27a8326c7b5165212141568d
Soumya Ranjan Moharana <soumyaranjanmoharana8659@gmail.com>
2026-05-21 06:30:45 +0000

    added adding todo functionality

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app= Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///todos.db"  #retrieving the db file
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False #Not needed as we never track if the object was modified or not and it's by default on (not of critical priority)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False #Not needed as we never track if the object was modified or not and it's by default on (not of critical priority) 
+#«app.cofig.... False» is on standstill 
 db.init_app(app)
 
 with app.app_context():
@@ -16,3 +17,12 @@ with app.app_context():
 def index():
     todos= Todo.query.order_by(Todo.created.desc()).all()
    return render_templates("index.html", todos= todos)
+
+@app.route("/add", methods= ["POST"])
+def add_todo():
+    user_input= request.form.get("task")
+    if bool(task)== True:
+        todo= Todo(task=user_input)
+        db.session.add(todo)
+        db.session.commit()
+    return redirect(url_for("index"))
