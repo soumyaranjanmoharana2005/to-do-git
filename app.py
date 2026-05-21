3e73adacfcfb387fcb38683e0aeef7687487435f
Soumya Ranjan Moharana <soumyaranjanmoharana8659@gmail.com>
2026-05-21 08:57:30 +0000

    Final commit with errors fixed and timestamp functionality added

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from flask import Flask, render_template, request, redirect, url_for
 app= Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///todos.db"  #retrieving the db file
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False #Not needed as we never track if the object was modified or not and it's by default on (not of critical priority) 
-#«app.cofig.... False» is on standstill 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False #Desired as we never track if the object was modified or not and it's by default on (not of critical priority) 
 db.init_app(app)
 
 with app.app_context():
@@ -16,12 +15,12 @@ with app.app_context():
 @app.route("/")
 def index():
     todos= Todo.query.order_by(Todo.created.desc()).all()
-   return render_templates("index.html", todos= todos)
+    return render_template("index.html", todos= todos)
 
 @app.route("/add", methods= ["POST"])
 def add_todo():
     user_input= request.form.get("task")
-    if bool(task)== True:
+    if bool(user_input)== True:
         todo= Todo(task=user_input)
         db.session.add(todo)
         db.session.commit()
@@ -35,3 +34,12 @@ def toggle_todo(id):
         db.session.commit()
     return redirect(url_for("index"))
 
+@app.route("/delete/<int:id>", methods= ["POST"])
+def delete_todo(id):
+    todo= Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+if __name__== "__main__":
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-from flask_sqalchemy import SQLAlchemy
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
     
 db =SQLAlchemy()
 
-class Todo(db.model):
+class Todo(db.Model):
     __tablename__= "Todos"
-    id = db.Column(db.integer, primary_key= True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key= True, autoincrement=True)
     task= db.Column(db.String, nullable=False) #to avoid blank tasks
     done= db.Column(db.Integer, default=0)
-    created= db.Column(db.DateTime, default= datetime.timezone.utc) #datetime.utcnow is deprecated and now has been replaced by datetime.timezone.utc
+    created= db.Column(db.DateTime, default= lambda:datetime.now(timezone.utc)) #datetime.utcnow is deprecated and now has been replaced by datetime.timezone.utc
